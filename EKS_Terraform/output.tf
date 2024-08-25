a6bd9de2e0744630823c5cc43147587ea837e30e
Mathew Joseph <100591950+pythonkid2@users.noreply.github.com>
2024-08-26 00:04:40 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.mega_project.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.mega_project.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.mega_project_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.mega_project_subnet[*].id
 }
